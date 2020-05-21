cec8a56db41f49bc36f8b1e8ba50d1ea351d7516
acendan <65690085+acendan@users.noreply.github.com>
2020-05-20 20:11:47 -0400

    Add files via upload

---
+++ Find Replace by Name/Find Replace in Marker Names.lua
@@ -0,0 +1,53 @@
+--[[
+ * Find Replace in Marker Names
+ * By Aaron Cendan
+ * May 2020
+ * Prompts user to replace part of a marker's name with
+ * new text if marker name contains search criteria.
+--]]
+
+function findReplace() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+	local retval, num_markers, num_regions = reaper.CountProjectMarkers( 0 )
+	local num_total = num_markers + num_regions
+	
+	if num_markers > 0 then
+		-- Store user input for search and replace strings
+		local ret,user_input = reaper.GetUserInputs( "Find & Replace in Marker Names",  2,
+						   "Text to Search For,Text to Replace With,extrawidth=100","")
+		if not ret then return end
+		local search_string, replace_string = user_input:match("([^,]+),([^,]+)")
+	
+		-- Loop through all markers
+		local i = 0
+		while i < num_total do
+			
+			local retval, isrgn, pos, rgnend, name, markrgnindexnumber, color = reaper.EnumProjectMarkers3( 0, i )
+
+			if not isrgn then
+				if string.find(name, search_string) then
+					local new_name = string.gsub( name, search_string, replace_string)
+					reaper.SetProjectMarkerByIndex( 0, i, isrgn, pos, rgnend, markrgnindexnumber, new_name, color )
+				end
+			end
+
+			i = i + 1
+		end
+	else
+		reaper.ShowMessageBox("Project has no markers!","Find/Replace", 0)
+		
+	end
+
+	reaper.Undo_EndBlock("Find and Replace", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+end
+
+reaper.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+findReplace() -- Execute your main function
+
+reaper.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
