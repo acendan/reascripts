b9035aba8d1630bc02e81460bd13208dfd1ab0d5
Aaron Cendan <aaron.cendan@gmail.com>
2020-08-06 22:30:57 -0400

    Update acendan_Lua Utilities.lua

--- Development/acendan_Lua Utilities.lua
+++ Development/acendan_Lua Utilities.lua
@@ -561,6 +561,27 @@ function setRenderSettings(t)
 end
 
 
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~~~ REAPACK ~~~~~~~~~~~~
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Open ReaPack About page for this script
+function help()
+  if not reaper.ReaPack_GetOwner then
+    reaper.MB('This feature requires ReaPack v1.2 or newer.', script_name, 0)
+    return
+  end
+  local owner = reaper.ReaPack_GetOwner(({reaper.get_action_context()})[2])
+  if not owner then
+    reaper.MB(string.format(
+      'This feature is unavailable because "%s" was not installed using ReaPack.',
+      script_name), script_name, 0)
+    return
+  end
+  reaper.ReaPack_AboutInstalledPackage(owner)
+  reaper.ReaPack_FreeEntry(owner)
+end
+
+
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
