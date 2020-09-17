03c6305e7cad1e83dc9e3cfd831a240cad11480b
Aaron Cendan <aaron.cendan@gmail.com>
2020-09-17 16:47:41 -0400

    Update acendan_Insert new track respect folders.lua

--- Tracks Properties/acendan_Insert new track respect folders.lua
+++ Tracks Properties/acendan_Insert new track respect folders.lua
@@ -1,6 +1,6 @@
 -- @description Insert New Track Respect Folders
 -- @author Aaron Cendan
--- @version 1.1
+-- @version 1.2
 -- @metapackage
 -- @provides
 --   [main] . > acendan_Insert new track respect folders.lua
@@ -31,30 +31,51 @@ function main()
     local sel_track = reaper.GetSelectedTrack(0,0)
     local sel_track_idx = reaper.GetMediaTrackInfo_Value( sel_track, "IP_TRACKNUMBER" )
     
-    -- Check to see if it's the parent
-    if not reaper.GetParentTrack( sel_track ) then
-      -- THIS TRACK IS THE PARENT OF A FOLDER
+    local folder_depth = reaper.GetMediaTrackInfo_Value( sel_track, "I_FOLDERDEPTH" )
+    local folder_depth_prev_track = 0
+    if sel_track_idx > 1 then
+      folder_depth_prev_track = reaper.GetMediaTrackInfo_Value( reaper.GetTrack(0, sel_track_idx - 2), "I_FOLDERDEPTH" )
+    end
+    
+    --dbg(tostring("Selected track folder depth: " .. folder_depth))
+    --dbg(tostring("Previous track folder depth: " .. folder_depth_prev_track))
+    
+    -- Normal track right after the last track in a nested folder
+    if folder_depth == 0 and folder_depth_prev_track < 0 then
       -- Insert new track below selected
       reaper.InsertTrackAtIndex( sel_track_idx, true )
       
       -- Set new track color
       local new_track = reaper.GetTrack(0, sel_track_idx)
       reaper.SetMediaTrackInfo_Value( new_track, "I_CUSTOMCOLOR", reaper.GetMediaTrackInfo_Value( sel_track, "I_CUSTOMCOLOR" ) )
-    
-    else
-      -- NOT THE PARENT, INSERT AFTER SELECTED
-      -- Insert new track above selected
-      reaper.InsertTrackAtIndex( sel_track_idx - 1, true )
+
+    -- Folder parent
+    elseif folder_depth == 1 then
+      -- Insert new track below selected
+      reaper.InsertTrackAtIndex( sel_track_idx, true )
       
       -- Set new track color
-      local new_track = reaper.GetTrack(0, sel_track_idx - 1)
+      local new_track = reaper.GetTrack(0, sel_track_idx)
       reaper.SetMediaTrackInfo_Value( new_track, "I_CUSTOMCOLOR", reaper.GetMediaTrackInfo_Value( sel_track, "I_CUSTOMCOLOR" ) )
-      
-      -- Move new track below originally selected track
-      reaper.SetOnlyTrackSelected( sel_track )
-      reaper.ReorderSelectedTracks( sel_track_idx - 1, 2 )
+     
+    -- Normal track, or last track in folder/nested folder
+    elseif folder_depth <= 0 then
+       -- Insert new track above selected
+       reaper.InsertTrackAtIndex( sel_track_idx - 1, true )
+       
+       -- Set new track color
+       local new_track = reaper.GetTrack(0, sel_track_idx - 1)
+       reaper.SetMediaTrackInfo_Value( new_track, "I_CUSTOMCOLOR", reaper.GetMediaTrackInfo_Value( sel_track, "I_CUSTOMCOLOR" ) )
+       
+       -- Move new track below originally selected track
+       reaper.SetOnlyTrackSelected( sel_track )
+       reaper.ReorderSelectedTracks( sel_track_idx - 1, 2 )
+       
+       -- Select new track
+       reaper.SetOnlyTrackSelected( new_track )
+       
     end
-    
+
   else
     -- Insert track at end of project if none selected
     reaper.InsertTrackAtIndex( reaper.CountTracks( 0 ), true )
