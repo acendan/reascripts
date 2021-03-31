751a4fc11a8f76d792f2d639cd19e45b461ef2ea
Aaron Cendan <aaron.cendan@gmail.com>
2021-03-31 17:20:01 -0400

    Extend regions left

---
+++ Regions/acendan_Extend regions in time selection left edge by length.lua
@@ -0,0 +1,80 @@
+-- @description Extend Regions in Time Selection
+-- @author Aaron Cendan
+-- @version 1.0
+-- @metapackage
+-- @provides
+--   [main] . > acendan_Extend regions in time selection by length.lua
+-- @link https://aaroncendan.me
+-- @about
+--   * Pretty self explanatory, input a number and it will extend your region by that length. 
+--   * Negative numbers will shrink regions!
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~ GLOBAL VARS ~~~~~~~~~~
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+-- Get this script's name and directory
+local script_name = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
+local script_directory = ({reaper.get_action_context()})[2]:sub(1,({reaper.get_action_context()})[2]:find("\\[^\\]*$"))
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function main()
+  -- Loop through regions in time selection
+  local ret, num_markers, num_regions = reaper.CountProjectMarkers( 0 )
+  local num_total = num_markers + num_regions
+  local start_time_sel, end_time_sel = reaper.GetSet_LoopTimeRange(0,0,0,0,0);
+  
+  if num_regions > 0 then
+    if start_time_sel ~= end_time_sel then
+      -- Single field
+      local ret_input, extension = reaper.GetUserInputs( "Extend Regions By Length", 1, "Length (seconds)", "1.0" )
+      if not ret_input then return end
+      
+      local i = 0
+      while i < num_total do
+        local retval, isrgn, pos, rgnend, name, markrgnindexnumber, color = reaper.EnumProjectMarkers3( 0, i )
+        if isrgn then
+          if pos >= start_time_sel and rgnend <= end_time_sel then
+            reaper.SetProjectMarker3( 0, markrgnindexnumber, isrgn, pos - extension, rgnend, name, color )
+          end
+        end
+        i = i + 1
+      end
+    else
+      msg("You need to make a time selection!")
+    end
+  else
+    msg("Project has no regions!")
+  end
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~~ UTILITIES ~~~~~~~~~~~
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Deliver messages and add new line in console
+function dbg(dbg)
+  reaper.ShowConsoleMsg(dbg .. "\n")
+end
+
+-- Deliver messages using message box
+function msg(msg)
+  reaper.MB(msg, script_name, 0)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+reaper.PreventUIRefresh(1)
+
+reaper.Undo_BeginBlock()
+
+main()
+
+reaper.Undo_EndBlock(script_name,-1)
+
+reaper.PreventUIRefresh(-1)
+
+reaper.UpdateArrange()
