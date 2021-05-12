e458497e633fc9cc635554ebff7163031045871f
Aaron Cendan <aaron.cendan@gmail.com>
2021-05-11 21:57:32 -0400

    Update acendan_Lua Utilities.lua

--- Development/acendan_Lua Utilities.lua
+++ Development/acendan_Lua Utilities.lua
@@ -1,6 +1,6 @@
 -- @description ACendan Lua Utilities
 -- @author Aaron Cendan
--- @version 4.5
+-- @version 4.6
 -- @metapackage
 -- @provides
 --   [main] . > acendan_Lua Utilities.lua
@@ -1289,6 +1289,87 @@ function acendan.fileToTable(filename)
   table.insert(t, "")
   io.close(file)
   return t
+end
+
+-- Convert table input to file, each entry = new line
+function acendan.tableToFile(  tbl,filename )
+  -- declare local variables
+  --// exportstring( string )
+  --// returns a "Lua" portable version of the string
+  local function exportstring( s )
+    return string.format("%q", s)
+  end
+  
+  local charS,charE = "   ","\n"
+  local file,err = io.open( filename, "wb" )
+  if err then return err end
+
+  -- initiate variables for save procedure
+  local tables,lookup = { tbl },{ [tbl] = 1 }
+  file:write( "return {"..charE )
+
+  for idx,t in ipairs( tables ) do
+     file:write( "-- Table: {"..idx.."}"..charE )
+     file:write( "{"..charE )
+     local thandled = {}
+
+     for i,v in ipairs( t ) do
+        thandled[i] = true
+        local stype = type( v )
+        -- only handle value
+        if stype == "table" then
+           if not lookup[v] then
+              table.insert( tables, v )
+              lookup[v] = #tables
+           end
+           file:write( charS.."{"..lookup[v].."},"..charE )
+        elseif stype == "string" then
+           file:write(  charS..exportstring( v )..","..charE )
+        elseif stype == "number" then
+           file:write(  charS..tostring( v )..","..charE )
+        end
+     end
+
+     for i,v in pairs( t ) do
+        -- escape handled values
+        if (not thandled[i]) then
+        
+           local str = ""
+           local stype = type( i )
+           -- handle index
+           if stype == "table" then
+              if not lookup[i] then
+                 table.insert( tables,i )
+                 lookup[i] = #tables
+              end
+              str = charS.."[{"..lookup[i].."}]="
+           elseif stype == "string" then
+              str = charS.."["..exportstring( i ).."]="
+           elseif stype == "number" then
+              str = charS.."["..tostring( i ).."]="
+           end
+        
+           if str ~= "" then
+              stype = type( v )
+              -- handle value
+              if stype == "table" then
+                 if not lookup[v] then
+                    table.insert( tables,v )
+                    lookup[v] = #tables
+                 end
+                 file:write( str.."{"..lookup[v].."},"..charE )
+              elseif stype == "string" then
+                 file:write( str..exportstring( v )..","..charE )
+              elseif stype == "number" then
+                 file:write( str..tostring( v )..","..charE )
+              end
+           end
+        end
+     end
+     file:write( "},"..charE )
+  end
+  file:write( "}" )
+  file:close()
 end
 
 -- Get web interface info from REAPER.ini // returns Table
