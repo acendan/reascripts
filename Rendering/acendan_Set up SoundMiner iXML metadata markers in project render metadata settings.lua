2864af50dd834b870ff7e58b196d061f288b6b2f
Aaron Cendan <aaron.cendan@gmail.com>
2021-05-19 22:15:47 -0400

    Update acendan_Set up SoundMiner iXML metadata markers in project render metadata settings.lua

--- Rendering/acendan_Set up SoundMiner iXML metadata markers in project render metadata settings.lua
+++ Rendering/acendan_Set up SoundMiner iXML metadata markers in project render metadata settings.lua
@@ -1,6 +1,6 @@
 -- @description SoundMiner iXML Metadata Render Settings
 -- @author Aaron Cendan
--- @version 1.0
+-- @version 1.1
 -- @metapackage
 -- @provides
 --   [main] . > acendan_Set up SoundMiner iXML metadata markers in project render metadata settings.lua
@@ -47,12 +47,22 @@ iXML["IXML:USER:OPENTIER"]       = "OpenTier"
 -- Other globals
 local script_name = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
 local script_directory = ({reaper.get_action_context()})[2]:sub(1,({reaper.get_action_context()})[2]:find("\\[^\\]*$"))
-local ini_section = "reaper_explorer"
-local dbg = false
 
 function main()
   for k, v in pairs(iXML) do
-    local ret, str = reaper.GetSetProjectInfo_String( 0, "RENDER_METADATA", k .. "|$marker(" .. v .. ")", true )
+    if v == "RecType" then
+      local ret, str = reaper.GetSetProjectInfo_String( 0, "RENDER_METADATA", k .. "|$bitdepth/$sampleratekk", true)
+    elseif v == "ReleaseDate" then
+      local ret, str = reaper.GetSetProjectInfo_String( 0, "RENDER_METADATA", k .. "|$date", true)
+    elseif v == "LongID" then
+      local ret, str = reaper.GetSetProjectInfo_String( 0, "RENDER_METADATA", k .. "|$marker(CatID)", true)
+    elseif v == "Source" then
+      local ret, str = reaper.GetSetProjectInfo_String( 0, "RENDER_METADATA", k .. "|$marker(Show)", true)
+    elseif v == "Artist" then
+      local ret, str = reaper.GetSetProjectInfo_String( 0, "RENDER_METADATA", k .. "|$marker(Designer)", true)
+    else
+      local ret, str = reaper.GetSetProjectInfo_String( 0, "RENDER_METADATA", k .. "|$marker(" .. v .. ")", true )
+    end
   end
 end
 
