e61bf62c80c700e3eef087da5fd2d9e205b94e3b
Aaron Cendan <aaron.cendan@gmail.com>
2021-08-31 17:10:50 -0400

    Added JSFX Support

--- README.md
+++ README.md
@@ -116,7 +116,9 @@ If you would like to import the remainder of my scripts, those that are not pack
  - Utility\acendan_Bounce In Place
  - Various\acendan_Auto generate scripts to open web interfaces
  - Various\acendan_GUI_Display first selected item properties
+ - Various\acendan_Insert random FX plugin on selected track
  - Various\acendan_Move unused projects MediaFiles in selected directory to separate folder
+ - Various\acendan_Open file from path in project notes
  - Various\acendan_Randomly deliver affirmations nvk_Game Audio Affirmations once an hour
  - Various\acendan_UCS Renaming Tool - Media Explorer Filter
  - Various\acendan_Universal Category Renaming Tool
--- Various/acendan_Insert random FX plugin on selected track.lua
+++ Various/acendan_Insert random FX plugin on selected track.lua
@@ -1,10 +1,12 @@
 -- @description Random Track FX
 -- @author Aaron Cendan
--- @version 1.0
+-- @version 1.1
 -- @metapackage
 -- @provides
 --   [main] .
 -- @link https://ko-fi.com/acendan_
+-- @changelog
+-- + Added JSFX support
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- ~~~~~~~~~~~ GLOBAL VARS ~~~~~~~~~~
@@ -26,28 +28,43 @@ if reaper.file_exists( acendan_LuaUtils ) then dofile( acendan_LuaUtils ); if no
 function main()
   local win, sep = acendan.getOS()
   local vst_file = reaper.GetResourcePath() .. sep .. "reaper-vstplugins64.ini"
-  local vsts = {}
+  local jsfx_file = reaper.GetResourcePath() .. sep .. "reaper-jsfx.ini"
+  local plugins = {}
   if reaper.file_exists(vst_file) then
-    -- Fetch VSTs
-    local vst_tbl = acendan.fileToTable(vst_file)
-    for _, v in pairs(vst_tbl) do
-      local vst = v:gsub(" %(.*","")
-      if vst:find(",") and not vst:find("<SHELL>") then
-        vst = vst:sub(vst:find(",[^,]*$")+1)
-        vsts[#vsts+1]=vst
+    if reaper.file_exists(jsfx_file) then
+    
+      -- Fetch plugins from vst_file
+      local vst_tbl = acendan.fileToTable(vst_file)
+      for _, v in pairs(vst_tbl) do
+        local vst = v:gsub(" %(.*","")
+        if vst:find(",") and not vst:find("<SHELL>") then
+          vst = vst:sub(vst:find(",[^,]*$")+1)
+          plugins[#plugins+1]=vst
+        end
       end
-    end
-
-    -- Loop through selected tracks
-    local num_sel_tracks = reaper.CountSelectedTracks( 0 )
-    if num_sel_tracks > 0 then
-      for i = 0, num_sel_tracks-1 do
-        local track = reaper.GetSelectedTrack(0,i)
-        reaper.TrackFX_AddByName(track,vsts[math.random(#vsts)],false,-1)
-        reaper.TrackFX_Show(track, reaper.TrackFX_GetCount(track)-1,3)
+      
+      -- Fetch plugins from jsfx_file
+      local jsfx_tbl = acendan.fileToTable(jsfx_file)
+      for _, j in pairs(jsfx_tbl) do
+        if j:find("NAME") and j:find("JS: ") then
+          local jsfx = j:sub(j:find("JS: "),-2):gsub("JS: ","JS:")
+          plugins[#plugins+1]=jsfx
+        end
+      end
+  
+      -- Loop through selected tracks
+      local num_sel_tracks = reaper.CountSelectedTracks( 0 )
+      if num_sel_tracks > 0 then
+        for i = 0, num_sel_tracks-1 do
+          local track = reaper.GetSelectedTrack(0,i)
+          reaper.TrackFX_AddByName(track,plugins[math.random(#plugins)],false,-1)
+          reaper.TrackFX_Show(track, reaper.TrackFX_GetCount(track)-1,3)
+        end
+      else
+        acendan.msg("No tracks selected!")
       end
     else
-      acendan.msg("No tracks selected!")
+      acendan.msg("Unable to find reaper-jsfx.ini file!")
     end
   else
     acendan.msg("Unable to find reaper-vstplugins64.ini file!")
