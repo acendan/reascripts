ef52d8a34fb5508ff73003163eca30cd325d557c
Aaron Cendan <aaron.cendan@gmail.com>
2022-03-01 11:37:13 -0800

    Update acendan_Add marker to end of selected items prompt for name.lua

--- Markers/acendan_Add marker to end of selected items prompt for name.lua
+++ Markers/acendan_Add marker to end of selected items prompt for name.lua
@@ -1,10 +1,12 @@
 -- @description Add Marker End Items
 -- @author Aaron Cendan
--- @version 1.0
+-- @version 1.1
 -- @metapackage
 -- @provides
 --   [main] . > acendan_Add marker to end of selected items prompt for name.lua
 -- @link https://aaroncendan.me
+-- @changelog
+--  # Added support for take markers for Jay
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- ~~~~~~ USER CONFIG - EDIT ME ~~~~~
@@ -33,11 +35,11 @@ function main()
   local num_sel_items = reaper.CountSelectedMediaItems(0)
   if num_sel_items > 0 then
     -- Prompt for user input
-    local ret_input, user_input = reaper.GetUserInputs( "Item End Markers", 2,
-                              "Marker Name,Marker Pos Offset" .. ",extrawidth=100",
-                              "end,0.0" )
+    local ret_input, user_input = reaper.GetUserInputs( "Item End Markers", 3,
+                              "Marker Name,Marker Pos Offset,Project (p) or Take (t) Marker" .. ",extrawidth=100",
+                              "end,0.0,t" )
     if not ret_input then return end
-    local mkr_name, pos_off = user_input:match("([^,]+),([^,]+)")
+    local mkr_name, pos_off, proj_or_take = user_input:match("([^,]+),([^,]+),([^,]+)")
     pos_off = tonumber(pos_off)
     
   
@@ -47,7 +49,14 @@ function main()
       local item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH" )
       local item_end = item_start + item_len
       
-      reaper.AddProjectMarker( 0, 0, item_end + pos_off, item_end + pos_off, mkr_name, i )
+      if proj_or_take == "t" then
+        -- Add as take marker
+        reaper.SetTakeMarker(reaper.GetActiveTake(item),-1,mkr_name,(item_end + pos_off) - item_start)
+        
+      else
+        -- Add as project marker
+        reaper.AddProjectMarker( 0, 0, item_end + pos_off, item_end + pos_off, mkr_name, i )
+      end
     end
   else
     acendan.msg("No items selected!")
