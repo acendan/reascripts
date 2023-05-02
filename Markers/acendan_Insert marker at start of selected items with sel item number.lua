4e61a0d813b080544f88e90e140ddc17ff457f86
Aaron Cendan <aaron.cendan@gmail.com>
2023-05-01 21:30:03 -0700

    Update acendan_Insert marker at start of selected items with sel item number.lua

--- Markers/acendan_Insert marker at start of selected items with sel item number.lua
+++ Markers/acendan_Insert marker at start of selected items with sel item number.lua
@@ -1,26 +1,33 @@
 -- @description Insert marker at start of selected items with number
 -- @author Aaron Cendan
--- @version 1.2
+-- @version 1.3
 -- @metapackage
 -- @provides
 --   [main] . > acendan_Insert marker at start of selected items with sel item number.lua
 -- @link https://aaroncendan.me
 -- @changelog
---   Aligned enumeration with marker and val
+--   Skip position if already used
+
+-- Load lua utilities
+acendan_LuaUtils = reaper.GetResourcePath()..'/Scripts/ACendan Scripts/Development/acendan_Lua Utilities.lua'
+if reaper.file_exists( acendan_LuaUtils ) then dofile( acendan_LuaUtils ); if not acendan or acendan.version() < 6.2 then acendan.msg('This script requires a newer version of ACendan Lua Utilities. Please run:\n\nExtensions > ReaPack > Synchronize Packages',"ACendan Lua Utilities"); return end else reaper.ShowConsoleMsg("This script requires ACendan Lua Utilities! Please install them here:\n\nExtensions > ReaPack > Browse Packages > 'ACendan Lua Utilities'"); return end
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- ~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function insertMarkers()
   reaper.Undo_BeginBlock()
-  
+  local mkr_pos_tbl = {}
   local num_sel_items = reaper.CountSelectedMediaItems(0)
   if num_sel_items > 0 then
     for i=1, num_sel_items do
       local item = reaper.GetSelectedMediaItem( 0, i-1 )
       local item_start = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
       local take = reaper.GetActiveTake( item )
-      reaper.AddProjectMarker( 0, 0, item_start, item_start, "#" .. tostring(i), i )
+      if not acendan.tableContainsVal(mkr_pos_tbl, item_start) then
+        reaper.AddProjectMarker( 0, 0, item_start, item_start, "#" .. tostring(i), i )
+        mkr_pos_tbl[#mkr_pos_tbl+1] = item_start
+      end
     end
   else
     reaper.MB("No items selected!","Insert Markers", 0)
