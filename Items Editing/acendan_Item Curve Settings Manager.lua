71c650c685d343dc28c5aba206a6bcd34e9fe820
Aaron Cendan <aaron.cendan@gmail.com>
2023-09-22 08:51:35 -0700

    Lua Utilities cleanup

--- Items Editing/acendan_Item Curve Settings Manager.lua
+++ Items Editing/acendan_Item Curve Settings Manager.lua
@@ -1,6 +1,6 @@
 -- @description Item Curve Settings Manager (ImGui)
 -- @author Aaron Cendan
--- @version 1.1
+-- @version 1.2
 -- @metapackage
 -- @provides
 --   [main] .
@@ -8,10 +8,10 @@
 -- @about
 --   # Item curve settings manager
 -- @changelog
---   * Minor code cleanup, no functional changes
+--   * Dependency on acendan_LuaUtils 7.1+
 
 local acendan_LuaUtils = reaper.GetResourcePath()..'/Scripts/ACendan Scripts/Development/acendan_Lua Utilities.lua'
-if reaper.file_exists( acendan_LuaUtils ) then dofile( acendan_LuaUtils ); if not acendan or acendan.version() < 6.6 then acendan.msg('This script requires a newer version of ACendan Lua Utilities. Please run:\n\nExtensions > ReaPack > Synchronize Packages',"ACendan Lua Utilities"); return end else reaper.ShowConsoleMsg("This script requires ACendan Lua Utilities! Please install them here:\n\nExtensions > ReaPack > Browse Packages > 'ACendan Lua Utilities'"); return end
+if reaper.file_exists( acendan_LuaUtils ) then dofile( acendan_LuaUtils ); if not acendan or acendan.version() < 7.1 then acendan.msg('This script requires a newer version of ACendan Lua Utilities. Please run:\n\nExtensions > ReaPack > Synchronize Packages',"ACendan Lua Utilities"); return end else reaper.ShowConsoleMsg("This script requires ACendan Lua Utilities! Please install them here:\n\nExtensions > ReaPack > Browse Packages > 'ACendan Lua Utilities'"); return end
 if not reaper.ImGui_Key_0() then acendan.msg("This script requires the ReaImGui API, which can be installed from:\n\nExtensions > ReaPack > Browse packages...") return end
   
   
@@ -35,7 +35,7 @@ function init()
   ctx = reaper.ImGui_CreateContext(SCRIPT_NAME, reaper.ImGui_ConfigFlags_DockingEnable())
   reaper.ImGui_SetNextWindowSize(ctx, WINDOW_SIZE.width, WINDOW_SIZE.height)
   
-  widgets = {
+  wgt = {
     fade_in = GetPreviousValue("fade_in", 0.0),
     fade_out = GetPreviousValue("fade_out", 0.0),
     continuous = false,
@@ -47,22 +47,18 @@ function main()
   local rv, open = reaper.ImGui_Begin(ctx, SCRIPT_NAME, true, WINDOW_FLAGS)
   if not rv then return open end
   
-  if reaper.ImGui_BeginChild(ctx, "main") then
-    AddSlider("Fade In", "fade_in")
-    HelpMarker("Ctrl+Click to input value.")
-    AddSlider("Fade Out", "fade_out")
-    reaper.ImGui_Spacing(ctx)
-    
-    AddButton("Selected Items", UpdateSelItems)
-    reaper.ImGui_SameLine(ctx)
-    AddButton("All Items", UpdateAllItems)
-    HelpMarker("Apply current slider settings.")
-    
-    rv, widgets.continuous = reaper.ImGui_Checkbox(ctx, "Continuous?", widgets.continuous)
-    HelpMarker("Updates items constantly with most recently clicked button.")
-
-    reaper.ImGui_EndChild(ctx)
-  end
+  AddSlider("Fade In", "fade_in")
+  acendan.ImGui_HelpMarker("Ctrl+Click to input value.")
+  AddSlider("Fade Out", "fade_out")
+  reaper.ImGui_Spacing(ctx)
+  
+  AddButton("Selected Items", UpdateSelItems)
+  reaper.ImGui_SameLine(ctx)
+  AddButton("All Items", UpdateAllItems)
+  acendan.ImGui_HelpMarker("Apply current slider settings.")
+  
+  rv, wgt.continuous = reaper.ImGui_Checkbox(ctx, "Continuous?", wgt.continuous)
+  acendan.ImGui_HelpMarker("Updates items constantly with most recently clicked button.")
 
   reaper.ImGui_End(ctx)
   if open then reaper.defer(main) else reaper.ImGui_DestroyContext(ctx) end
@@ -74,27 +70,27 @@ end
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function AddSlider(name, key)
-  rv, widgets[key] = reaper.ImGui_SliderDouble(ctx, name, widgets[key], -1.0, 1.0, "Curve = %.2f", SLIDER_FLAGS)
-  if rv then reaper.SetExtState(SCRIPT_NAME, key, widgets[key], true) end
+  rv, wgt[key] = reaper.ImGui_SliderDouble(ctx, name, wgt[key], -1.0, 1.0, "Curve = %.2f", SLIDER_FLAGS)
+  if rv then reaper.SetExtState(SCRIPT_NAME, key, wgt[key], true) end
 end
 
 function AddButton(name, callback)
-  local is_last_pressed = widgets.last_pressed == name
-  local enabled = widgets.last_pressed == "" or is_last_pressed or not widgets.continuous
-  local enabled_cont = is_last_pressed and widgets.continuous
+  local is_last_pressed = wgt.last_pressed == name
+  local enabled = wgt.last_pressed == "" or is_last_pressed or not wgt.continuous
+  local enabled_cont = is_last_pressed and wgt.continuous
   
   if enabled_cont then callback() end
   if not enabled then reaper.ImGui_BeginDisabled(ctx) end
   if reaper.ImGui_Button(ctx, name) then
-    widgets.last_pressed = name
+    wgt.last_pressed = name
     callback()
   end
   if not enabled then reaper.ImGui_EndDisabled(ctx) end
 end
 
 function UpdateItem(item)
-  reaper.SetMediaItemInfo_Value( item, "D_FADEINDIR", widgets.fade_in)
-  reaper.SetMediaItemInfo_Value( item, "D_FADEOUTDIR", widgets.fade_out)
+  reaper.SetMediaItemInfo_Value( item, "D_FADEINDIR", wgt.fade_in)
+  reaper.SetMediaItemInfo_Value( item, "D_FADEOUTDIR", wgt.fade_out)
 end
 
 function UpdateSelItems()
@@ -117,18 +113,6 @@ end
 
 function GetPreviousValue(key, default)
   return reaper.HasExtState(SCRIPT_NAME, key) and reaper.GetExtState(SCRIPT_NAME, key) or default
-end
-
-function HelpMarker(desc)
-  reaper.ImGui_SameLine(ctx)
-  reaper.ImGui_TextDisabled(ctx, '(?)')
-  if reaper.ImGui_IsItemHovered(ctx) then
-    reaper.ImGui_BeginTooltip(ctx)
-    reaper.ImGui_PushTextWrapPos(ctx, reaper.ImGui_GetFontSize(ctx) * 18.0)
-    reaper.ImGui_Text(ctx, desc)
-    reaper.ImGui_PopTextWrapPos(ctx)
-    reaper.ImGui_EndTooltip(ctx)
-  end
 end
 
 
