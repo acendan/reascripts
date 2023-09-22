41186e9610b6b97167f938b0e81e40527a01a23e
Aaron Cendan <aaron.cendan@gmail.com>
2023-09-22 08:31:41 -0700

    Update acendan_Item Curve Settings Manager.lua

--- Items Editing/acendan_Item Curve Settings Manager.lua
+++ Items Editing/acendan_Item Curve Settings Manager.lua
@@ -1,104 +1,64 @@
 -- @description Item Curve Settings Manager (ImGui)
 -- @author Aaron Cendan
--- @version 1.0
+-- @version 1.1
 -- @metapackage
 -- @provides
 --   [main] .
 -- @link https://ko-fi.com/acendan_
 -- @about
 --   # Item curve settings manager
+-- @changelog
+--   * Minor code cleanup, no functional changes
 
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- ~~~~~~ USER CONFIG - EDIT ME ~~~~~
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- ~~~~~~~~~~~ GLOBAL VARS ~~~~~~~~~~
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
--- Get this script's name and directory
-local script_name = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
-local script_directory = ({reaper.get_action_context()})[2]:sub(1,({reaper.get_action_context()})[2]:find("\\[^\\]*$"))
-
--- Load lua utilities
-acendan_LuaUtils = reaper.GetResourcePath()..'/Scripts/ACendan Scripts/Development/acendan_Lua Utilities.lua'
+local acendan_LuaUtils = reaper.GetResourcePath()..'/Scripts/ACendan Scripts/Development/acendan_Lua Utilities.lua'
 if reaper.file_exists( acendan_LuaUtils ) then dofile( acendan_LuaUtils ); if not acendan or acendan.version() < 6.6 then acendan.msg('This script requires a newer version of ACendan Lua Utilities. Please run:\n\nExtensions > ReaPack > Synchronize Packages',"ACendan Lua Utilities"); return end else reaper.ShowConsoleMsg("This script requires ACendan Lua Utilities! Please install them here:\n\nExtensions > ReaPack > Browse Packages > 'ACendan Lua Utilities'"); return end
+if not reaper.ImGui_Key_0() then acendan.msg("This script requires the ReaImGui API, which can be installed from:\n\nExtensions > ReaPack > Browse packages...") return end
+  
+  
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~~ CONSTANTS ~~~~~~~~~~~
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
--- Separator for tempo map serialization
-local sep = "||"
+local SCRIPT_NAME = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
+local SCRIPT_DIR = ({reaper.get_action_context()})[2]:sub(1,({reaper.get_action_context()})[2]:find("\\[^\\]*$"))
+
+local WINDOW_SIZE = { width = 300, height = 130 }
+local WINDOW_FLAGS = reaper.ImGui_WindowFlags_NoCollapse()
+local SLIDER_FLAGS = reaper.ImGui_SliderFlags_AlwaysClamp()
+
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- ~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function init()
-  -- Confirm user has ImGui installed
-  if not reaper.ImGui_Key_0() then acendan.msg("This script requires the ReaImGui API, which can be installed from:\n\nExtensions > ReaPack > Browse packages...") return end
-    
-  ctx = reaper.ImGui_CreateContext(script_name, reaper.ImGui_ConfigFlags_DockingEnable())
+  ctx = reaper.ImGui_CreateContext(SCRIPT_NAME, reaper.ImGui_ConfigFlags_DockingEnable())
+  reaper.ImGui_SetNextWindowSize(ctx, WINDOW_SIZE.width, WINDOW_SIZE.height)
   
   widgets = {
-    fade_in = reaper.HasExtState(script_name, "fade_in") and reaper.GetExtState(script_name, "fade_in") or 0.0,
-    fade_out = reaper.HasExtState(script_name, "fade_out") and reaper.GetExtState(script_name, "fade_out") or 0.0,
+    fade_in = GetPreviousValue("fade_in", 0.0),
+    fade_out = GetPreviousValue("fade_out", 0.0),
     continuous = false,
     last_pressed = ""
   }
-  
-  WINDOW_FLAGS = reaper.ImGui_WindowFlags_None()
-  WINDOW_FLAGS = WINDOW_FLAGS | reaper.ImGui_WindowFlags_NoCollapse()
-  WINDOW_SIZE = { width = 300, height = 130 }
-  reaper.ImGui_SetNextWindowSize(ctx, WINDOW_SIZE.width, WINDOW_SIZE.height)
-  
-  SLIDER_FLAGS = reaper.ImGui_SliderFlags_AlwaysClamp()
-   
-  -- ReaImGui_Demo
-  -- Using those as a base value to create width/height that are factor of the size of our font
-  TEXT_BASE_WIDTH  = reaper.ImGui_CalcTextSize(ctx, 'A')
-  TEXT_BASE_HEIGHT = reaper.ImGui_GetTextLineHeightWithSpacing(ctx)
-  FLT_MIN, FLT_MAX = reaper.ImGui_NumericLimits_Float()
-  
-  main()
 end
 
 function main()
-  local rv, open = reaper.ImGui_Begin(ctx, script_name, true, WINDOW_FLAGS)
+  local rv, open = reaper.ImGui_Begin(ctx, SCRIPT_NAME, true, WINDOW_FLAGS)
   if not rv then return open end
   
   if reaper.ImGui_BeginChild(ctx, "main") then
-    rv, widgets.fade_in = reaper.ImGui_SliderDouble(ctx, "Fade In", widgets.fade_in, -1.0, 1.0, "Curve = %.2f", SLIDER_FLAGS)
-    if rv then reaper.SetExtState(script_name, "fade_in", widgets.fade_in, true) end
+    AddSlider("Fade In", "fade_in")
     HelpMarker("Ctrl+Click to input value.")
-    
-    rv, widgets.fade_out = reaper.ImGui_SliderDouble(ctx, "Fade Out", widgets.fade_out, -1.0, 1.0, "Curve = %.2f", SLIDER_FLAGS)
-    if rv then reaper.SetExtState(script_name, "fade_out", widgets.fade_out, true) end
-    
+    AddSlider("Fade Out", "fade_out")
     reaper.ImGui_Spacing(ctx)
     
-    local continuous_selected = widgets.continuous and widgets.last_pressed == "Selected Items"
-    local continuous_all = widgets.continuous and widgets.last_pressed == "All Items"
-    
-    -- Selected items
-    if continuous_selected then UpdateSelItems() end
-    if continuous_all then reaper.ImGui_BeginDisabled(ctx) end
-    if reaper.ImGui_Button(ctx, "Selected Items") then
-      widgets.last_pressed = "Selected Items"
-      UpdateSelItems()
-    end
-    if continuous_all then reaper.ImGui_EndDisabled(ctx) end
-
-    -- All items
-    if continuous_all then UpdateAllItems() end
-    if continuous_selected then reaper.ImGui_BeginDisabled(ctx) end
+    AddButton("Selected Items", UpdateSelItems)
     reaper.ImGui_SameLine(ctx)
-    if reaper.ImGui_Button(ctx, "All Items") then
-      widgets.last_pressed = "All Items"
-      UpdateAllItems()
-    end
-    if continuous_selected then reaper.ImGui_EndDisabled(ctx) end
+    AddButton("All Items", UpdateAllItems)
     HelpMarker("Apply current slider settings.")
     
     rv, widgets.continuous = reaper.ImGui_Checkbox(ctx, "Continuous?", widgets.continuous)
-    if rv and not widgets.continuous then widgets.last_pressed = "" end
     HelpMarker("Updates items constantly with most recently clicked button.")
 
     reaper.ImGui_EndChild(ctx)
@@ -108,9 +68,29 @@ function main()
   if open then reaper.defer(main) else reaper.ImGui_DestroyContext(ctx) end
 end
 
+
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- ~~~~~~~~~~~~ UTILITIES ~~~~~~~~~~~
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function AddSlider(name, key)
+  rv, widgets[key] = reaper.ImGui_SliderDouble(ctx, name, widgets[key], -1.0, 1.0, "Curve = %.2f", SLIDER_FLAGS)
+  if rv then reaper.SetExtState(SCRIPT_NAME, key, widgets[key], true) end
+end
+
+function AddButton(name, callback)
+  local is_last_pressed = widgets.last_pressed == name
+  local enabled = widgets.last_pressed == "" or is_last_pressed or not widgets.continuous
+  local enabled_cont = is_last_pressed and widgets.continuous
+  
+  if enabled_cont then callback() end
+  if not enabled then reaper.ImGui_BeginDisabled(ctx) end
+  if reaper.ImGui_Button(ctx, name) then
+    widgets.last_pressed = name
+    callback()
+  end
+  if not enabled then reaper.ImGui_EndDisabled(ctx) end
+end
 
 function UpdateItem(item)
   reaper.SetMediaItemInfo_Value( item, "D_FADEINDIR", widgets.fade_in)
@@ -135,19 +115,25 @@ function UpdateAllItems()
   end
 end
 
+function GetPreviousValue(key, default)
+  return reaper.HasExtState(SCRIPT_NAME, key) and reaper.GetExtState(SCRIPT_NAME, key) or default
+end
+
 function HelpMarker(desc)
   reaper.ImGui_SameLine(ctx)
   reaper.ImGui_TextDisabled(ctx, '(?)')
   if reaper.ImGui_IsItemHovered(ctx) then
     reaper.ImGui_BeginTooltip(ctx)
-    reaper.ImGui_PushTextWrapPos(ctx, reaper.ImGui_GetFontSize(ctx) * 35.0)
+    reaper.ImGui_PushTextWrapPos(ctx, reaper.ImGui_GetFontSize(ctx) * 18.0)
     reaper.ImGui_Text(ctx, desc)
     reaper.ImGui_PopTextWrapPos(ctx)
     reaper.ImGui_EndTooltip(ctx)
   end
 end
 
+
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- ~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 init()
+main()
